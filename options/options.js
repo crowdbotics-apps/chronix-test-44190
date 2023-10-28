45e15c5a0670ffc871652738c16dc72754d17c4a
Crowdbotics <team@crowdbotics.com>
2023-10-28 16:45:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chronix-test-44190",
+  url: "https://chronix-test-44190.botics.co",
+  api: "https://chronix-test-44190.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
